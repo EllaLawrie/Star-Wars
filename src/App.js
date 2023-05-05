d545390473eec1ec441ddf3ba46f7f35dd5174c5
EllaLawrie <lawrieella002@gmail.com>
2023-05-05 05:03:07 +0100

    update of movie detail

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import MovieContent from './components/MovieContent.jsx'
-
+import { Routes, Route } from 'react-router-dom'
+import MovieDetail from './components/MovieDetail.jsx'
 const App = () => {
   return (
-    <div className='App'>
-      <MovieContent />
-    </div>
+    <Routes>
+      <Route element={<MovieContent />} path='/' />
+      <Route element={<MovieDetail />} path='/movies/:id' />
+    </Routes>
   )
 }
 
---
+++ src/components/MovieDetail.jsx
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import { useQuery } from 'react-query'
+import { useNavigate, useParams } from 'react-router-dom'
+
+// create a component for the movie details page
+const MovieDetail = () => {
+  const { id } = useParams()
+  const getMovieDetail = async (movieId) => {
+    try {
+      return (await axios.get(`https://swapi.dev/api/films/${movieId}/`)).data
+    } catch (error) {
+      return error
+    }
+  }
+
+  const { data: movie, isFetching } = useQuery(['movie-detail', id], () =>
+    getMovieDetail(id)
+  )
+
+  const navigate = useNavigate()
+  return (
+    <div>
+      {isFetching && <p className='movie-detail-desc'>Loading...</p>}
+      {movie && (
+        <section className='detail-wrapper'>
+          <div className='detail-card'>
+            <h1 className='movie-detail-header'>{movie.title}</h1>
+            <p className='movie-detail-desc'>{movie.opening_crawl}</p>
+            <button onClick={() => navigate(-1)}>Back</button>
+          </div>
+        </section>
+      )}
+    </div>
+  )
+}
+export default MovieDetail
