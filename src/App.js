b338df75322ccc5d32586962b66e5c3d9a380003
EllaLawrie <lawrieella002@gmail.com>
2023-05-05 07:43:32 +0100

    adjusted logo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 import MovieContent from './components/MovieContent.jsx'
 import { Routes, Route } from 'react-router-dom'
 import MovieDetail from './components/MovieDetail.jsx'
+import Logo from './components/Logo.jsx'
 const App = () => {
   return (
-    <Routes>
-      <Route element={<MovieContent />} path='/' />
-      <Route element={<MovieDetail />} path='/movies/:id' />
-    </Routes>
+    <>
+      <Logo />
+      <Routes>
+        <Route element={<MovieContent />} path='/' />
+        <Route element={<MovieDetail />} path='/movies/:id' />
+      </Routes>
+    </>
   )
 }
 
